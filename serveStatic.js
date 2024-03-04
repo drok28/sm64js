3c5826688a7062ec5f9400365259b96b1e732fc8
uuphoria <155935644+uuphoria2@users.noreply.github.com>
2024-03-04 10:52:03 -0600

    sm64js 2020 lore (easiest to use)

--- serveStatic.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const http = require('http')
-const server = http.Server(app)
-const port = 80
-
-app.use(express.static(__dirname + '/dist'))
-server.listen(port, () => { console.log('Serving Files') })
